92295c6d9d01a04b91472463605e9bce8fa567a5
Muhammad Aamir <amirsavvy>
2019-03-19 13:16:38 +0500

    named tuple

--- app.py
+++ app.py
@@ -178,6 +178,35 @@ def display_info(name, age):
 display_info("Amir Khan", 30)
 
 
+"""
+Decorators with arguments 
+"""
+
+
+def prefix_decorator(prefix):
+
+    def decorator_function(original_function):
+
+        def wrapper_function(*args, **kwargs):
+            print(prefix, "Executed before", original_function.__name__)
+            result = original_function(*args, **kwargs)
+            print(prefix, "Executed after", original_function.__name__)
+            return result
+
+        return wrapper_function
+
+    return decorator_function
+
+
+@prefix_decorator('TESTING:')
+def display_info(name, age):
+    print("display_info ran with arguments ({}, {})". format(name, age))
+
+
+print('Display info using Decorator with arguments')
+display_info('Amir Savvy', 26)
+
+
 
 
 
